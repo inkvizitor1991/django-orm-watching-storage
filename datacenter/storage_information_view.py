4ee13ba250b61dddf69433a7d32854b11cd8fe98
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-09-14 16:14:02 +0300

    removed the extra code

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,27 +9,25 @@ from .models import is_visit_long
 
 
 def storage_information_view(request):
-    visits = Visit.objects.all()
+    visits = Visit.objects.filter(leaved_at=None)
     minutes = 60
     non_closed_visits = []
     for visit in visits:
-        exit_date = visit.leaved_at
-        if not exit_date:
-            arrival_date = visit.entered_at
-            visitor_name = visit.passcard
-            date_now = django.utils.timezone.localtime()
-            duration_seconds = get_duration(arrival_date, date_now)
-            location_time = format_duration(duration_seconds)
-            visit_long = is_visit_long(duration_seconds, minutes)
-            arrival_date = visit.entered_at
-            non_closed_visits.append(
-                {
-                    'who_entered': visitor_name,
-                    'entered_at': arrival_date,
-                    'duration': location_time,
-                    'is_strange': visit_long,
-                }
-            )
+        arrival_date = visit.entered_at
+        visitor_name = visit.passcard
+        date_now = django.utils.timezone.localtime()
+        duration_seconds = get_duration(arrival_date, date_now)
+        location_time = format_duration(duration_seconds)
+        visit_long = is_visit_long(duration_seconds, minutes)
+        arrival_date = visit.entered_at
+        non_closed_visits.append(
+            {
+                'who_entered': visitor_name,
+                'entered_at': arrival_date,
+                'duration': location_time,
+                'is_strange': visit_long,
+            }
+        )
 
     context = {
         'non_closed_visits': non_closed_visits,
